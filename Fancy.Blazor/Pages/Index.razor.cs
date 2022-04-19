44dcabf962de6e1bc7222f868fcaf015abd484ae
Dmitry Suschevsky <d.suschevsky.personal@gmail.com>
2022-04-19 18:24:19 +0300

    возможность загрузки трат

---
+++ Fancy.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,62 @@
+﻿using Fancy.Blazor.Expenses;
+using Fancy.Blazor.Shared;
+using Fancy.Domain.Expenses;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Fancy.Blazor.Pages
+{
+    public partial class Index
+    {
+        private string? _expenseName;
+        private decimal _expenseAmount;
+        private RepeatTypeComponent _repeatTypeComponent = null!;
+        private List<Expense> _expenses = new List<Expense>();
+        private IList<MonthExpense> _monthExpenses = Array.Empty<MonthExpense>();
+
+        [Inject]
+        public ExpenseCalculator ExpenseCalculator { get; set; } = null!;
+
+        [Inject]
+        public ExpensesStore ExpensesStore { get; set; } = null!;
+
+        [Inject]
+        public ExpenseImportExportService ExpenseImportExportService { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _expenses = await ExpensesStore.LoadExpensesAsync();
+            Calculate();
+        }
+
+        private async ValueTask Add()
+        {
+            var ex = new Expense(_expenseName ?? string.Empty, _expenseAmount, _repeatTypeComponent.GetRepeatType());
+            _expenses.Add(ex);
+            await ExpensesStore.SaveExpensesAsync(_expenses);
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            _monthExpenses = ExpenseCalculator.Calculate(_expenses).ToList();
+        }
+
+        private async ValueTask Remove(Expense expense)
+        {
+            _expenses.Remove(expense);
+            await ExpensesStore.SaveExpensesAsync(_expenses);
+            Calculate();
+        }
+
+        private async ValueTask UploadFiles(InputFileChangeEventArgs e)
+        {
+            var result = await ExpenseImportExportService.LoadAsync(e.File);
+            if (result.Count == 0)
+                return;
+
+            _expenses.AddRange(result);
+            Calculate();
+        }
+    }
+}
--- Fancy.Test/ExpenseCalculatorTest.cs
+++ Fancy.Test/ExpenseCalculatorTest.cs
@@ -16,7 +16,7 @@ public class ExpenseCalculatorTest
         var calculator = new ExpenseCalculator();
         
         var actualExpenses = calculator.CalculateWithSafetyBag(expenses, safeBagPlan, currentAmountOfMoneyInSafeBag).ToList();
-        
+
         Assert.Equal(expectExpenses.Count, actualExpenses.Count);
         Assert.Equal(expectExpenses, actualExpenses);
     }
@@ -48,7 +48,9 @@ public class ExpenseCalculatorTest
                         new("Еда", 300),
                         new("Квартира", 400),
                         new("Комуналка", 100),
+
                         new("Отпуск", 250),
+
                         new("Покупка квартиры", 3334),
                         new("Покупка машины", 1167),
                     }
@@ -56,43 +58,26 @@ public class ExpenseCalculatorTest
                 
                 yield return new object[]
                 {
-                    new List<Expense>
-                    {
-                    },
+                    new List<Expense> {},
                     new SafeBagPlan(TimeSpan.FromDays(183)),
                     6000,
-                    new List<MonthExpense>
-                    {
-                    }
+                    new List<MonthExpense>{}
                 };
                 
                 yield return new object[]
                 {
-                    new List<Expense>
-                    {
-                        new("Еда", 300, MultipleInMonthRepeatType.Once),
-                    },
+                    new List<Expense>{ new("Еда", 300, MultipleInMonthRepeatType.Once) },
                     new SafeBagPlan(TimeSpan.FromDays(183)),
                     1800,
-                    new List<MonthExpense>
-                    {
-                        new("Еда", 300)
-                    }
+                    new List<MonthExpense> { new("Еда", 300) }
                 };
                 
                 yield return new object[]
                 {
-                    new List<Expense>
-                    {
-                        new("Еда", 300, MultipleInMonthRepeatType.Once),
-                    },
+                    new List<Expense> { new("Еда", 300, MultipleInMonthRepeatType.Once), },
                     new SafeBagPlan(TimeSpan.FromDays(183)),
                     1799,
-                    new List<MonthExpense>
-                    {
-                        new("Еда", 300),
-                        new(SafeBagPlan.FundraisingName, 1),
-                    }
+                    new List<MonthExpense> { new("Еда", 300), new(SafeBagPlan.FundraisingName, 1), }
                 };
             }
         }
